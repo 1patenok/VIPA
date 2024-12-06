b93ae50886eaee7bdc2f2eaf6a37ecaaf0255af5
Nikita Trifonov <trifanya.dev@gmail.com>
2024-12-06 04:50:42 +0300

    Написаны unit-тесты для AuthenticationController и AuthenticationService

--- src/main/java/com/example/vipa/dto/ClientDetailsDto.java
+++ src/main/java/com/example/vipa/dto/ClientDetailsDto.java
@@ -3,12 +3,14 @@ package com.example.vipa.dto;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.experimental.Accessors;
 
 import java.util.Date;
 
 @Getter
 @Setter
 @ToString
+@Accessors(chain = true)
 public class ClientDetailsDto {
     private String name;
     private String surname;
--- src/main/java/com/example/vipa/model/Client.java
+++ src/main/java/com/example/vipa/model/Client.java
@@ -4,6 +4,7 @@ package com.example.vipa.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.Accessors;
 
 import java.util.Date;
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.Objects;
 @Setter
 @Entity
 @Table(name = "client")
+@Accessors(chain = true)
 public class Client {
     @Id
     @Column(name = "client_id")
