04670307b9008c055b43d5cf1d8d3c118436ad3e
Vadim Ipatov <ipatovvadim@yandex.ru>
2024-12-19 00:50:22 +0300

    Сделал фотографии для категорий и для пользователей

--- src/main/resources/db/migration/V1_2/V1_2_1__category_table_testdata.sql
+++ src/main/resources/db/migration/V1_2/V1_2_1__category_table_testdata.sql
@@ -1,12 +1,12 @@
 INSERT INTO category(name, image)
 VALUES
-('Электроника', '/uploads/category-images/electronics.jpg'),
-('Одежда и обувь', '/uploads/category-images/clothing.jpg'),
-('Автомобили', '/uploads/category-images/cars.jpg'),
-('Недвижимость', '/uploads/category-images/real_estate.jpg'),
-('Дом и сад', '/uploads/category-images/home_garden.jpg'),
-('Детские товары', '/uploads/category-images/kids.jpg'),
-('Спорт и отдых', '/uploads/category-images/sports.jpg'),
-('Книги и учебные материалы', '/uploads/category-images/books.jpg'),
-('Красота и здоровье', '/uploads/category-images/beauty.jpg'),
-('Услуги', '/uploads/category-images/services.jpg');
+('Электроника', '/images/category-images/Electronics.jpg'),
+('Одежда и обувь', '/images/category-images/ClothAndShoes.jpg'),
+('Автомобили', '/images/category-images/cars.jpg'),
+('Недвижимость', '/images/category-images/real_estate.jpg'),
+('Дом и сад', '/images/category-images/home_garden.jpg'),
+('Детские товары', '/images/category-images/kids.jpg'),
+('Спорт и отдых', '/images/category-images/sports.jpg'),
+('Книги и учебные материалы', '/images/category-images/books.jpg'),
+('Красота и здоровье', '/images/category-images/beauty.jpg'),
+('Услуги', '/images/category-images/services.jpg');
